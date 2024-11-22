b6808a696053c120473181e5e7685d2bd96b2877
Sathya Seelan <sathyaseelangunasekar@gmail.com>
2024-11-22 21:01:04 +0530

    coinzila

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,18 @@ const Layout = ({ children, title = "AniPick | Anime Recommendations" }) => {
     <>
       <Head>
         <title>{title}</title> {/* Dynamically set the title */}
-        <link rel="icon" href="https://raw.githubusercontent.com/SathyaSeelanG/assets/refs/heads/main/Projects/AniPick/AniPick_logo.png" /> {/* Same favicon for all pages */}
+        <link
+          rel="icon"
+          href="https://raw.githubusercontent.com/SathyaSeelanG/assets/refs/heads/main/Projects/AniPick/AniPick_logo.png"
+        />{" "}
+        {/* Same favicon for all pages */}
+        <meta name="coinzilla" content="b6a5b9418431355bf54609d99fe12f2d" />
+        {/* Coinzilla Script */}
+        <script
+          async
+          src="https://example.coinzilla-script-url.com"
+          data-id="your-coinzilla-id"
+        ></script>
       </Head>
       <Header />
       <main>{children}</main>
